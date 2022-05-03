d7899e3052e416fb1346f00f890e1fd274cba525
Lukas Kreibig <78150333+lukaskreibig@users.noreply.github.com>
2022-05-03 18:19:39 +0200

    finish react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import './App.scss';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
 
 const App = () => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<any>("");
 
-  const [data, setData] = useState<any>(null)
-  const [loading, setLoading] = useState<boolean>(false)
-  const [error, setError] = useState<any>("")
-
-  useEffect(() :void =>  {
+  useEffect((): void => {
     const getData = async () => {
       try {
         const response = await fetch(
@@ -21,73 +20,79 @@ const App = () => {
         let actualData = await response.json();
         setData(actualData);
         setError(null);
-      } catch(err:any) {
+      } catch (err: any) {
         setError(err.message);
         setData(null);
       } finally {
         setLoading(false);
-      }  
-    }
-    getData()
-  }, [])
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <div className="App">
       <h1>Canonical</h1>
       {loading && <i className="p-icon--spinner u-animation--spin"></i>}
-      {error && (
-        <div>{`Error fetching the data - ${error}`}</div>
-      )}
-
+      {error && <div>{`Error fetching the data - ${error}`}</div>}
 
       <div className="col-12">
-        
-
-    
         <div className="row u-equal-height u-clearfix">
-        {data &&
-          data.map(({title, date, link, featured_media, _embedded}:any, index:number) => (
-            
-            <div className="col-4 blog-p-card--post u-align--left" key={index}> 
-            
+          {data &&
+            data.map(
+              (
+                { title, date, link, featured_media, _embedded }: any,
+                index: number
+              ) => (
+                <div
+                  className="col-4 blog-p-card--post u-align--left"
+                  key={index}
+                >
+                  <header className="blog-p-card">
+                    <h5 className="p-muted-heading u-no-margin--bottom">
+                      {_embedded["wp:term"][2][0]
+                        ? _embedded["wp:term"][2][0].name
+                        : "Ubuntu"}
+                    </h5>
+                  </header>
 
-                <header className="blog-p-card">
-                <h5 className="p-muted-heading u-no-margin--bottom"> {_embedded["wp:term"][2][0] ? (_embedded["wp:term"][2][0].name) : "Ubuntu"} </h5>
-                </header>
-                
-                <div className="p-card__inner"> 
+                  <div className="p-card__inner">
+                    <img
+                      className="p-card__image"
+                      src={featured_media}
+                      alt="Blog"
+                      loading="lazy"
+                    />
 
-                <img className="p-card__image" src={featured_media} alt="Blog" loading="lazy" />
-                
-                  <h3 className="p-heading--4">
-                  <a href={link}>
-                  {title.rendered}
-                  </a>
-                  </h3>
-                 
-                  
-                  <p>
-                  <em>
-                  by <a href={_embedded.author[0].link}>{_embedded.author[0].name}</a> on {new Date(date).toLocaleString('default', { day: 'numeric', month: 'long', year: 'numeric' })}
-                  </em>
-                  </p>
-                  
+                    <h3 className="p-heading--4">
+                      <a href={link}>{title.rendered}</a>
+                    </h3>
+
+                    <p>
+                      <em>
+                        by{" "}
+                        <a href={_embedded.author[0].link}>
+                          {_embedded.author[0].name}
+                        </a>{" "}
+                        on{" "}
+                        {new Date(date).toLocaleString("default", {
+                          day: "numeric",
+                          month: "long",
+                          year: "numeric",
+                        })}
+                      </em>
+                    </p>
                   </div>
                   <p className="p-card__footer">
-                  {_embedded["wp:term"][0][0].name}
-
+                    {_embedded["wp:term"][0][0].name}
                   </p>
-                  
-                  </div>
-
-          
-
-          ))}
-          </div>
-            </div>
-            </div>
-
+                </div>
+              )
+            )}
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
